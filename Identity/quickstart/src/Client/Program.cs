7e91b66758237672e49f2998c62325422cbcd6b1
wzq <907066892@qq.com>
2020-03-18 14:32:12 +0800

    身份验证以及 代码混淆工具

---
+++ Identity/quickstart/src/Client/Program.cs
@@ -0,0 +1,54 @@
+﻿using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            var disco = client.GetDiscoveryDocumentAsync("http://localhost:5000");
+            if (disco.Result.IsError)
+            {
+                Console.WriteLine(disco.Result.Error);
+                return;
+            }
+            var tokenResponse = client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest()
+            {
+                Address = disco.Result.TokenEndpoint,
+                ClientId = "client",
+                ClientSecret = "123",
+                Scope = "api1"
+            });
+
+            if (tokenResponse.Result.IsError)
+            {
+                Console.WriteLine(tokenResponse.Result.Error);
+                return;
+            }
+            Console.WriteLine(tokenResponse.Result.Json);
+
+
+            // call api
+            var clieTtwo = new HttpClient();
+            client.SetBearerToken(tokenResponse.Result.AccessToken);
+
+            var response =   client.GetAsync("http://localhost:5001/identity");
+            if (!response.Result.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.Result.StatusCode);
+            }
+            else
+            {
+                var content = response.Result.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content.Result));
+            }
+
+
+
+        }
+    }
+}
